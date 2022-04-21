3334db2821d941605b791a30a350115b356b02fc
Ari Gaskins <arigaskins.tech@gmail.com>
2022-04-20 22:32:00 -0400

    test dictionary

---
+++ anime_pandas.py
@@ -0,0 +1,10 @@
+import pandas as pd
+
+my_anime_list = {'7 seeds': ['currently watching', 'adventure', 'drama', 'mystery', 'romance', 'sci-fi'], 
+'Bakemonogatari': ['currently watching', 'mystery', 'romance', 'supernatural'], 'Banana Fish': ['action', 'adventure', 'drama'],
+'Black Clover': ['currently watching', 'action', 'comedy', 'fantasy'], 'Bleach': ['currently watching', 'action', 'adventure', 'fantasy'],
+'Cannon Busters': ['on hold', 'action', 'adventure', 'fantasy', 'sci-fi'], 'Fairy Tail': ['on hold', 'action', 'adventure', 'fantasy'],
+'High School DxD': ['on hold', 'comedy', 'romance', 'ecchi'], 'Mnemosyne': ['on hold', 'action', 'girls love', 'horror', 'sci-fi', 'supernatural'],
+'Nana': ['on hold', 'drama', 'romance', 'slice of life'], 'Abenobashi Mahou Shoutengai': ['plan to watch', 'comedy', 'fantasy', 'ecchi'],
+'Adachi to Shimamura': ['plan to watch', 'girls love', 'romance', 'slice of life'], 'Air Gear': ['plan to watch', 'action', 'comedy', 'sports', 'ecchi'],
+'Akibas Trip': ['plan to watch', 'action', 'comedy', 'supernatural', 'ecchi'], 'Aldnoah.Zero': ['plan to watch', 'action', 'sci-fi']}
